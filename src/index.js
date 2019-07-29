a27a31c09d4afb731fe76b57b7550f4374eee3a0
David Navarro <david.navastud@gmail.com>
2019-07-29 13:13:25 -0400

    Redux and Redux-Saga (#13)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
+import store from "./store";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
